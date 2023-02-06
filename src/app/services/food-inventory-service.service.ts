aff7fd3c9ce669f6a49c1a2f505dd8a3e91cb1c6
ravina-ghidode <ravina.ghidode@newvision-software.com>
2023-02-06 17:42:54 +0530

    Added Delete and Search

--- src/app/services/food-inventory-service.service.ts
+++ src/app/services/food-inventory-service.service.ts
@@ -4,20 +4,21 @@ import { map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodInventoryServiceService implements OnInit {
-
   apiUrl = environment.apiUrl;
-  constructor(private http : HttpClient) { }
-  ngOnInit(): void {
-    
-  }
-  getAllFood(){
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {}
+  getAllFood() {
     return this.http.get<any>(`${this.apiUrl}food-inventories`).pipe(
-      map((res:any)=>{
-        return res.data
+      map((res: any) => {
+        return res.data;
       })
-    )
+    );
+  }
+
+  deleteFood(id: number) {
+    return this.http.delete<any>(`${this.apiUrl}food-inventories/${id}`);
   }
 }
