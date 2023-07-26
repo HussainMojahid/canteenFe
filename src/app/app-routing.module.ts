230902384d0f186c4436ec6fb73df83a6b0fd553
SuhaniLariya <suhani.lariya@ecoa.christuniversity.in>
2023-07-26 15:21:42 +0530

    hello commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router';
+;
 import { BookingComponent } from './booking/booking.component';
 import { DashboardModalComponent } from './dashboard/dashboard-modal/dashboard-modal.component';
 import { FeedbackComponent } from './feedback/feedback.component';
